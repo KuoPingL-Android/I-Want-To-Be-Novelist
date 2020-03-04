d761e3058814130a65fbd41f6135c7a8b25fbb71
JimmyLau <kuopingl+1@gmail.com>
2020-03-04 18:07:37 +0800

    [refactor<HOMEWORKINPROGRESSITEMVIEWHOLDER>]: del unused code

--- app/src/main/java/studio/saladjam/iwanttobenovelist/homescene/viewholders/HomeWorkInProgressItemViewHolder.kt
+++ app/src/main/java/studio/saladjam/iwanttobenovelist/homescene/viewholders/HomeWorkInProgressItemViewHolder.kt
@@ -6,20 +6,23 @@ import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LifecycleRegistry
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.RecyclerView
+import studio.saladjam.iwanttobenovelist.Logger
 import studio.saladjam.iwanttobenovelist.databinding.ItemHomeV1WorkInProgressBinding
 import studio.saladjam.iwanttobenovelist.homescene.HomeSections
 import studio.saladjam.iwanttobenovelist.homescene.HomeViewModel
 import studio.saladjam.iwanttobenovelist.homescene.HomeWorkInProgressViewModel
 import studio.saladjam.iwanttobenovelist.repository.dataclass.Book
 
-class HomeWorkInProgressItemViewHolder (val binding: ItemHomeV1WorkInProgressBinding, val workInProgressViewModel: HomeWorkInProgressViewModel)
-    : RecyclerView.ViewHolder(binding.root), LifecycleOwner {
+class HomeWorkInProgressItemViewHolder (
+    val binding: ItemHomeV1WorkInProgressBinding,
+    private val workInProgressViewModel: HomeWorkInProgressViewModel)
+    : RecyclerView.ViewHolder(binding.root),
+    LifecycleOwner {
 
     /** LIFECYCLE */
     private val lifecycleRegistry = LifecycleRegistry(this)
 
     override fun getLifecycle(): Lifecycle {
-        Log.i("LIFECYCLE", "${lifecycleRegistry.currentState}")
         return lifecycleRegistry
     }
 
@@ -28,12 +31,10 @@ class HomeWorkInProgressItemViewHolder (val binding: ItemHomeV1WorkInProgressBin
     }
 
     fun onAttached() {
-        Log.i("FORMVIEWHOLDER", "onATTACHED")
         lifecycleRegistry.currentState = Lifecycle.State.STARTED
     }
 
     fun onDetached() {
-        Log.i("FORMVIEWHOLDER", "onCREATED")
         lifecycleRegistry.currentState = Lifecycle.State.CREATED
     }
 
